9e86dc7f58d4763558a5c1451bf81110c38c522e
Andre Bonnano <abdrebonnano@gmail.com>
2022-01-26 21:03:45 -0300

    ok

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -34,29 +34,42 @@ baseUrl = "http://localhost:3001/products"
     )
   }
 
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
+  }
+
+  readById(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Product>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
+  }
+
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
+  }
+
+  delete(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.delete<Product>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
+  }
+  
   errorHandler(e: any): Observable<any>{
     //console.log(e)
     this.showMessage('Aconteceu um erro inisperado!', true)
     return EMPTY
   }
 
-  read(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseUrl)
-  }
-
-  readById(id: string): Observable<Product> {
-    const url = `${this.baseUrl}/${id}`
-    return this.http.get<Product>(url)
-  }
-
-  update(product: Product): Observable<Product> {
-    const url = `${this.baseUrl}/${product.id}`
-    return this.http.put<Product>(url, product)
-  }
-
-  delete(id: string): Observable<Product> {
-    const url = `${this.baseUrl}/${id}`
-    return this.http.delete<Product>(url)
-  }
-
 }
+
